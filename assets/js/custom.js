053ec4839cce1f8d149d1909ef0a72dc257da18c
TouseefDesigns <mirza.zia121@gmail.com>
2021-09-27 23:32:43 -0700

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -187,8 +187,6 @@ $('#main-banner-carousel').owlCarousel({
 
 
 
-
-
 /*===========================
 ====== Porfolio Filter ======
 =============================*/
@@ -314,6 +312,42 @@ $('.counter').counterUp({
   delay: 10,
   time: 1000
 });
+
+
+
+/*===========================
+======= To Top ========
+=============================*/
+$(document).ready(function(e) {
+
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 200,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 100,
+    //grab the "back to top" link
+    $back_to_top = $('#back-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function() {
+    ($(this).scrollTop() > offset) ? $back_to_top.addClass('is-visible'):
+      $back_to_top.removeClass('is-visible is-fade-out');
+    if ($(this).scrollTop() > offset_opacity) {
+      $back_to_top.addClass('is-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event) {
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0,
+    }, scroll_top_duration);
+  });
+});
+
+
 
 
 
