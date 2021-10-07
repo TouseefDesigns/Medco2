ec54b1744390d4cd3d7c068c0981847c96781fd5
TouseefDesigns <mirza.zia121@gmail.com>
2021-10-06 23:43:06 -0700

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -312,29 +312,4 @@
 // window.onload = function () { document.body.scrollTop = document.documentElement.scrollTop = 0; };
 
 
-
-
-
-jQuery.event.special.touchstart = {
-  setup: function( _, ns, handle ) {
-      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
-  }
-};
-jQuery.event.special.touchmove = {
-  setup: function( _, ns, handle ) {
-      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
-  }
-};
-jQuery.event.special.wheel = {
-  setup: function( _, ns, handle ){
-      this.addEventListener("wheel", handle, { passive: true });
-  }
-};
-jQuery.event.special.mousewheel = {
-  setup: function( _, ns, handle ){
-      this.addEventListener("mousewheel", handle, { passive: true });
-  }
-};
-
-
 })(jQuery);
