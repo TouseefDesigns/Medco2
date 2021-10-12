ff59e8d806d6fd981ee1e80e2cd42f6ad4ce1396
TouseefDesigns <mirza.zia121@gmail.com>
2021-10-11 23:04:55 -0700

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,6 +24,18 @@
 
 
 
+  $('body').on('mouseenter mouseleave','.dropdown',function(e){
+    var _d=$(e.target).closest('.dropdown');
+    if (e.type === 'mouseenter')_d.addClass('show');
+    setTimeout(function(){
+      _d.toggleClass('show', _d.is(':hover'));
+      $('[data-toggle="dropdown"]', _d).attr('aria-expanded',_d.is(':hover'));
+    },300);
+  });
+  
+  /* this is not needed, just prevents page reload when a dd link is clicked */
+  $('.dropdown a').on('click tap', e => e.preventDefault())
+
   /*===========================
   ====== Porfolio Filter ======
   =============================*/
